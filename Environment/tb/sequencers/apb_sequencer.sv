8297da705f7f18790913f7e251e0ad3fc0212486
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-10 01:15:15 +0300

    Update apb_sequencer.sv

--- Environment/tb/sequencers/apb_sequencer.sv
+++ Environment/tb/sequencers/apb_sequencer.sv
@@ -1,1 +1,15 @@
 
+package seqr_pac;
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+import sequence_item::*;
+class apb_sequencer extends uvm_sequencer #(seq_item);
+    `uvm_component_utils(apb_sequencer)
+
+    function new(string name = "apb_sequencer" , uvm_component parent = null);
+        super.new(name,parent);
+    endfunction
+
+endclass 
+    
+endpackage
