d2dbd90a26518d85538814b8a766c4277097338c
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-10 01:15:43 +0300

    Update spi_sequencer.sv

--- Environment/tb/sequencers/spi_sequencer.sv
+++ Environment/tb/sequencers/spi_sequencer.sv
@@ -1,1 +1,14 @@
+package seqr_pac;
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+import sequence_item::*;
+class spi_sequencer extends uvm_sequencer #(seq_item);
+    `uvm_component_utils(spi_sequencer)
 
+    function new(string name = "spi_sequencer" , uvm_component parent = null);
+        super.new(name,parent);
+    endfunction
+
+endclass 
+    
+endpackage
