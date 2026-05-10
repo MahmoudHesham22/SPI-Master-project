678c7723ed8b9ef37dea6c9ea3b39bfb8c7d105f
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-10 14:06:59 +0300

    Update apb_monitor.sv

--- Environment/tb/monitors/apb_monitor.sv
+++ Environment/tb/monitors/apb_monitor.sv
@@ -1,30 +1,30 @@
 class apb_monitor extends uvm_monitor;
      `uvm_component_utils(apb_monitor)
-    seq_item item;
-    virtual apb_interface apb_test_vif;
-    uvm_analysis_port #(seq_item) mon_ap;
+    apb_sequence_item apb_item;
+    virtual apb_if apb_vif;
+    uvm_analysis_port #(apb_sequence_item) apb_mon_ap;
        
     function new(string name = "apb_monitor",uvm_component parent = null);
-            super.new(name,parent);
+        super.new(name,parent);
     endfunction //new()
         
     function void build_phase (uvm_phase phase);
-            super.build_phase(phase);
-            mon_ap=new("mon_ap",this);
+        super.build_phase(phase);
+        apb_mon_ap=new("apb_mon_ap",this);
     endfunction 
     
     task  run_phase(uvm_phase phase);
         super.run_phase(phase);
         forever begin
-            item = seq_item::type_id::create("item");
-            @(negedge apb_test_vif.clk);
-            item.PSEL = apb_test_vif.PSEL;
-            item.PENABLE = apb_test_vif.PENABLE;
-            item.PWRITE = apb_test_vif.PWRITE;
-            item.PADDR = apb_test_vif.PADDR;
-            item.PWDATA = apb_test_vif.PWDATA;
-            item.PRDATA = apb_test_vif.PRDATA;
-            mon_ap.write(item);
+            apb_item = apb_sequence_item::type_id::create("apb_item");
+            @(negedge apb_vif.clk);
+            apb_item.PSEL = apb_vif.PSEL;
+            apb_item.PENABLE = apb_vif.PENABLE;
+            apb_item.PWRITE = apb_vif.PWRITE;
+            apb_item.PADDR = apb_vif.PADDR;
+            apb_item.PWDATA = apb_vif.PWDATA;
+            apb_item.PRDATA = apb_vif.PRDATA;
+            apb_mon_ap.write(apb_item);
         end
     endtask //
-endclass //className extends superClass
+endclass 
