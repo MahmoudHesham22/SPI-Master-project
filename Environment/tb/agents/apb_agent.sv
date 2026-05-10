488c658c25e3009ccd0667cf5e9e684f10b51593
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-10 03:11:06 +0300

    Update apb_agent.sv

--- Environment/tb/agents/apb_agent.sv
+++ Environment/tb/agents/apb_agent.sv
@@ -1,1 +1,39 @@
+class apb_agent extends uvm_agent;
+    `uvm_component_utils(apb_agent)
+    apb_driver driver;
+    apb_monitor monitor;
+    apb_confg cfg;
+    sqr_class sqr;
+    uvm_analysis_port #(seq_item) agt_ap;
+        
+    function new(string name="apb_agent", uvm_component parent = null);
+            super.new(name,parent);
+    endfunction //new()
+        
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        monitor = apb_monitor::type_id::create("mon",this);
+        
+        if(!uvm_config_db #(apb_confg) :: get(this,"","CFG",cfg))
+        `uvm_fatal("build_phase","no");
+        
+        if(cfg.is_active==UVM_ACTIVE)
+            begin
+                driver = apb_driver::type_id::create("driver",this);
+                sqr = sqr_class::type_id::create("sqr",this);   
+            end
+        agt_ap = new("agt_ap",this);
+    endfunction
+        
+    function void connect_phase (uvm_phase phase);
+        super.connect_phase(phase);
+        monitor.apb_slave_test_vif=cfg.apb_slave_test_vif;
+        if(cfg.is_active==UVM_ACTIVE)
+            begin
+                driver.apb_slave_test_vif=cfg.apb_slave_test_vif;
+                driver.seq_item_port.connect(sqr.seq_item_export);    
+            end
 
+        monitor.mon_ap.connect(agt_ap);
+    endfunction 
+endclass //className extends superClass
