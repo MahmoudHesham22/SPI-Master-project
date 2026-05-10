aca28cff6ac22edfb5aba229db9c891ab0c16b4a
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-10 14:07:21 +0300

    Update spi_monitor.sv

--- Environment/tb/monitors/spi_monitor.sv
+++ Environment/tb/monitors/spi_monitor.sv
@@ -1,30 +1,29 @@
-
-class Spi_slave_monitor extends uvm_monitor;
-    `uvm_component_utils(Spi_slave_monitor)
-    seq_item item;
-    virtual Spi_slave_inter Spi_slave_test_vif;
-    uvm_analysis_port #(seq_item) mon_ap;
+class spi_monitor extends uvm_monitor;
+    `uvm_component_utils(spi_monitor)
+    spi_sequence_item item;
+    virtual spi_if spi_vif;
+    uvm_analysis_port #(spi_sequence_item) spi_mon_ap;
     
-    function new(string name = "Spi_slave_monitor",uvm_component parent = null);
-            super.new(name,parent);
+    function new(string name = "spi_monitor",uvm_component parent = null);
+        super.new(name,parent);
     endfunction //new()
         
     function void build_phase (uvm_phase phase);
-            super.build_phase(phase);
-            mon_ap=new("mon_ap",this);
+        super.build_phase(phase);
+        spi_mon_ap=new("spi_mon_ap",this);
     endfunction 
     
     task  run_phase(uvm_phase phase);
         super.run_phase(phase);
         forever begin
-            item = seq_item::type_id::create("item");
-            @(negedge Spi_slave_test_vif.clk);
-            item.SS_n = Spi_slave_test_vif.SS_n;
-            item.rst_n = Spi_slave_test_vif.rst_n;
-            item.tx_valid = Spi_slave_test_vif.tx_valid;
-            item.MOSI = Spi_slave_test_vif.MOSI;
-            item.tx_data = Spi_slave_test_vif.tx_data;
-            mon_ap.write(item);
+            item = spi_sequence_item::type_id::create("item");
+            @(negedge spi_vif.clk);
+            item.SS_n = spi_vif.SS_n;
+            item.rst_n = spi_vif.rst_n;
+            item.tx_valid = spi_vif.tx_valid;
+            item.MOSI = spi_vif.MOSI;
+            item.tx_data = spi_vif.tx_data;
+            spi_mon_ap.write(item);
         end
     endtask //
 endclass //className extends superClass
