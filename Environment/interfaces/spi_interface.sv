2d91e3c3fbd8b977b71a1fb9142b4fee1e4c6924
Hesham Nabil <91266833+Hesham-Nabil@users.noreply.github.com>
2026-05-10 13:10:44 +0300

    Define SPI interface with master, slave, and monitor modports

--- Environment/interfaces/spi_interface.sv
+++ Environment/interfaces/spi_interface.sv
@@ -1,1 +1,17 @@
+interface spi_if (input logic PCLK);
 
+    logic        SCLK;          
+    logic        MOSI;          
+    logic        MISO;          
+    logic [3:0]  SS_n;          
+    logic        IRQ;           
+
+    modport master  (input PCLK, MISO,
+                     output SCLK, MOSI, SS_n, IRQ);
+
+    modport slave   (input PCLK, SCLK, MOSI, SS_n, IRQ,
+                     output MISO);
+
+    modport monitor (input PCLK, SCLK, MOSI, MISO, SS_n, IRQ);
+
+endinterface : spi_if
