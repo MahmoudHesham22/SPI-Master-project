c7c886e075a19289242638eced99c8b872509154
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-10 14:06:15 +0300

    Update apb_driver.sv

--- Environment/tb/drivers/apb_driver.sv
+++ Environment/tb/drivers/apb_driver.sv
@@ -7,38 +7,31 @@
 // Description: This file contains the implementation of the APB driver class, which is responsible for driving the APB signals based on the sequence items received from the sequencer. The driver interacts with the APB
 //              slave interface to perform the necessary operations for the APB communication.
 //------------------------------------------------------------------------------
-
-package drive;
-    import uvm_pkg::*;
-    import cfg::*;
-    import sequence_item::*;
-    `include "uvm_macros.svh"
-    class Apb_driver extends uvm_driver#(seq_item);
-    `uvm_component_utils(Apb_driver)
-    parameter testcases = 20000;
-    seq_item item;
-    virtual Apb_slave_inter Apb_slave_test_vif;
+class apb_driver extends uvm_driver#(apb_sequence_item);
+    `uvm_component_utils(apb_driver)
+    apb_sequence_item apb_item;
+    virtual apb_if apb_vif;
         
-    function new(string name="Apb_driver",uvm_component parent = null);
-            super.new(name,parent);
+    function new(string name="apb_driver",uvm_component parent = null);
+        super.new(name,parent);
     endfunction //new()
     
     function void build_phase (uvm_phase phase);
         super.build_phase(phase);
     endfunction 
-        task run_phase (uvm_phase phase);
+
+    task run_phase (uvm_phase phase);
             super.run_phase(phase);
             forever begin
-                item = seq_item::type_id::create("item");
-                seq_item_port.get_next_item(item);
-                Apb_slave_test_vif.PSEL = item.PSEL;
-                Apb_slave_test_vif.PENABLE = item.PENABLE;
-                Apb_slave_test_vif.PWRITE = item.PWRITE;
-                Apb_slave_test_vif.PADDR = item.PADDR;
-                Apb_slave_test_vif.PWDATA = item.PWDATA;
-                @(negedge Apb_slave_test_vif.clk);
+                apb_item = apb_sequence_item::type_id::create("apb_item");
+                seq_item_port.get_next_item(apb_item);
+                apb_vif.PSEL = apb_item.PSEL;
+                apb_vif.PENABLE = apb_item.PENABLE;
+                apb_vif.PWRITE = apb_item.PWRITE;
+                apb_vif.PADDR = apb_item.PADDR;
+                apb_vif.PWDATA = apb_item.PWDATA;
+                @(negedge apb_vif.clk);
                 seq_item_port.item_done();
             end
         endtask //run_phase
     endclass //className extends superClass
-endpackage
