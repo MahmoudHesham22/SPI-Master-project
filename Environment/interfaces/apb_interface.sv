dfc79fa44e851264b9cfb9ba92240e1599da5902
Hesham Nabil <91266833+Hesham-Nabil@users.noreply.github.com>
2026-05-10 13:10:12 +0300

    Define APB interface with master, slave, and monitor modports

--- Environment/interfaces/apb_interface.sv
+++ Environment/interfaces/apb_interface.sv
@@ -1,1 +1,20 @@
+interface apb_if (input logic PCLK, input logic PRESETn);
 
+    logic        PSEL;
+    logic        PENABLE;
+    logic        PWRITE;
+    logic [7:0]  PADDR;
+    logic [31:0] PWDATA;
+    logic [31:0] PRDATA;
+    logic        PREADY;
+    logic        PSLVERR;
+
+    modport master  (input PCLK, PRESETn, PRDATA, PREADY, PSLVERR,
+                     output PSEL, PENABLE, PWRITE, PADDR, PWDATA);
+                     
+    modport slave   (input PCLK, PRESETn, PSEL, PENABLE, PWRITE, PADDR, PWDATA, 
+                     output PRDATA, PREADY, PSLVERR);
+
+    modport monitor (input PCLK, PRESETn, PSEL, PENABLE, PWRITE, PADDR, PWDATA, PRDATA, PREADY, PSLVERR);
+
+endinterface : apb_if
