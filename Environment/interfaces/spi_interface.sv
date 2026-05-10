572fa9168013d5998454ea44ebd5ab37feca026c
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-10 13:33:03 +0300

    Update spi_interface.sv

--- Environment/interfaces/spi_interface.sv
+++ Environment/interfaces/spi_interface.sv
@@ -1,17 +1,19 @@
-interface spi_if (input logic PCLK);
+interface apb_if (input logic PCLK);
+    
+    logic       PRESETn;
+    logic        PSEL;
+    logic        PENABLE;
+    logic        PWRITE;
+    logic [7:0]  PADDR;
+    logic [31:0] PWDATA;
+    logic [31:0] PRDATA;
+    logic        PREADY;
+    logic        PSLVERR;
 
-    logic        SCLK;          
-    logic        MOSI;          
-    logic        MISO;          
-    logic [3:0]  SS_n;          
-    logic        IRQ;           
+                     
+    modport DUT (input PCLK, PRESETn, PSEL, PENABLE, PWRITE, PADDR, PWDATA, 
+                     output PRDATA, PREADY, PSLVERR);
 
-    modport master  (input PCLK, MISO,
-                     output SCLK, MOSI, SS_n, IRQ);
+    modport monitor (input PCLK, PRESETn, PSEL, PENABLE, PWRITE, PADDR, PWDATA, PRDATA, PREADY, PSLVERR);
 
-    modport slave   (input PCLK, SCLK, MOSI, SS_n, IRQ,
-                     output MISO);
-
-    modport monitor (input PCLK, SCLK, MOSI, MISO, SS_n, IRQ);
-
-endinterface : spi_if
+endinterface : apb_if
