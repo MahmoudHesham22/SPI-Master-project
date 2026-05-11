0805e7e4e2de2e83a31b58d1b4a64254d4a795d0
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-11 16:45:27 +0300

    Add files via upload

--- Environment/tb/sequence items/apb_sequence_item.sv
+++ Environment/tb/sequence items/apb_sequence_item.sv
@@ -1,1 +1,21 @@
+import spi_master_pkg::*;
 
+class apb_sequence_item extends uvm_sequence_item;
+    `uvm_object_utils(apb_sequence_item)
+        
+    function new(string name = "apb_sequence_item");
+            super.new(name);
+    endfunction //new()
+
+    logic       PRESETn;
+    logic        PSEL;
+    logic        PENABLE;
+    logic        PWRITE;
+    logic [7:0]  PADDR;
+    logic [31:0] PWDATA;
+    logic [31:0] PRDATA;
+    logic        PREADY;
+    logic        PSLVERR;
+
+
+endclass
--- Environment/tb/sequence items/spi_sequence_item.sv
+++ Environment/tb/sequence items/spi_sequence_item.sv
@@ -1,1 +1,18 @@
+import spi_master_pkg::*;
 
+class spi_sequence_item extends uvm_sequence_item;
+    `uvm_object_utils(spi_sequence_item)
+        
+    function new(string name = "spi_sequence_item");
+            super.new(name);
+    endfunction //new()
+
+    logic      PRESETn;
+    logic        SCLK;          
+    logic        MOSI;          
+    logic        MISO;          
+    logic [3:0]  SS_n;          
+    logic        IRQ;  
+
+
+endclass
