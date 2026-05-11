ff599202fc20d8a8d2d1be42a6a9e736ba482fe8
Ahmeddhamdy5 <168377111+Ahmeddhamdy5@users.noreply.github.com>
2026-05-11 20:00:51 +0300

    Add localparam definitions for APB registers

--- Environment/tb/ref_model.sv
+++ Environment/tb/ref_model.sv
@@ -1,5 +1,16 @@
 class spi_ref_model;
 
+    // Register address parameters
+    localparam [7:0] APB_CTRL     = 8'h00;
+    localparam [7:0] APB_STATUS   = 8'h04;
+    localparam [7:0] APB_TX_DATA  = 8'h08;
+    localparam [7:0] APB_RX_DATA  = 8'h0C;
+    localparam [7:0] APB_CLK_DIV  = 8'h10;
+    localparam [7:0] APB_SS_CTRL  = 8'h14;
+    localparam [7:0] APB_INT_EN   = 8'h18;
+    localparam [7:0] APB_INT_STAT = 8'h1C;
+    localparam [7:0] APB_DELAY    = 8'h20;
+
     int error_count = 0;
 
     bit        reg_ctrl_en;
