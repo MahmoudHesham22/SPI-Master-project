da85eec85416d0a98bcca239710a2e69af3a4ab9
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-11 16:42:00 +0300

    Update apb_agent.sv

--- Environment/tb/agents/apb_agent.sv
+++ Environment/tb/agents/apb_agent.sv
@@ -1,10 +1,10 @@
 class apb_agent extends uvm_agent;
     `uvm_component_utils(apb_agent)
-    apb_driver apb_driver;
-    apb_monitor apb_monitor;
+    apb_driver apb_drvr;
+    apb_monitor apb_mntr;
     apb_config apb_cfg;
     apb_sequencer apb_sqr;
-    uvm_analysis_port #(apb_seq_item) apb_agt_ap;
+    uvm_analysis_port #(apb_sequence_item) apb_agt_ap;
         
     function new(string name="apb_agent", uvm_component parent = null);
             super.new(name,parent);
@@ -12,14 +12,14 @@ class apb_agent extends uvm_agent;
         
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        apb_monitor = apb_monitor::type_id::create("apb_monitor",this);
+        apb_mntr = apb_monitor::type_id::create("apb_monitor",this);
         
         if(!uvm_config_db #(apb_config) :: get(this,"","CFG",apb_cfg))
         `uvm_fatal("build_phase","error in getting config object");
         
         if(apb_cfg.is_active==UVM_ACTIVE)
             begin
-                apb_driver = apb_driver::type_id::create("apb_driver",this);
+                apb_drvr = apb_driver::type_id::create("apb_driver",this);
                 apb_sqr = apb_sequencer::type_id::create("apb_sqr",this);   
             end
         apb_agt_ap = new("apb_agt_ap",this);
@@ -27,13 +27,13 @@ class apb_agent extends uvm_agent;
         
     function void connect_phase (uvm_phase phase);
         super.connect_phase(phase);
-        apb_monitor.apb_slave_test_vif=apb_cfg.apb_slave_test_vif;
+        apb_mntr.apb_vif=apb_cfg.apb_vif;
         if(apb_cfg.is_active==UVM_ACTIVE)
             begin
-                apb_driver.apb_slave_vif = apb_cfg.apb_slave_vif;
-                apb_driver.seq_item_port.connect(apb_sqr.seq_item_export);    
+                apb_drvr.apb_vif = apb_cfg.apb_vif;
+                apb_drvr.seq_item_port.connect(apb_sqr.seq_item_export);    
             end
 
-        apb_monitor.apb_mon_ap.connect(apb_agt_ap);
+        apb_mntr.apb_mon_ap.connect(apb_agt_ap);
     endfunction 
 endclass //className extends superClass
