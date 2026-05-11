0a2d9a80bfcdc339d6683891ba0313059fdec849
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-11 16:42:28 +0300

    Update spi_agent.sv

--- Environment/tb/agents/spi_agent.sv
+++ Environment/tb/agents/spi_agent.sv
@@ -1,10 +1,10 @@
 
 class spi_agent extends uvm_agent;
     `uvm_component_utils(spi_agent)
-    spi_driver spi_driver;
-    spi_monitor spi_monitor;
+    spi_driver spi_drvr;
+    spi_monitor spi_mntr;
     spi_config spi_cfg;
-    spi_sequencer spi_sequencer;
+    spi_sequencer spi_sqr;
     uvm_analysis_port #(spi_sequence_item) spi_agt_ap;
         
     function new(string name="spi_agent", uvm_component parent = null);
@@ -13,28 +13,28 @@ class spi_agent extends uvm_agent;
         
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        spi_monitor = spi_monitor::type_id::create("spi_monitor",this);
+        spi_mntr = spi_monitor::type_id::create("spi_monitor",this);
         
         if(!uvm_config_db #(spi_config) :: get(this,"","CFG",spi_cfg))
         `uvm_fatal("build_phase","error in getting config object");
         
         if(spi_cfg.is_active==UVM_ACTIVE)
             begin
-            spi_driver = spi_driver::type_id::create("spi_driver",this);
-            spi_sequencer = spi_sequencer::type_id::create("spi_sequencer",this);   
+            spi_drvr = spi_driver::type_id::create("spi_driver",this);
+            spi_sqr = spi_sequencer::type_id::create("spi_sequencer",this);   
             end
         spi_agt_ap = new("spi_agt_ap",this);
     endfunction
         
     function void connect_phase (uvm_phase phase);
         super.connect_phase(phase);
-        spi_monitor.spi_slave_vif=spi_cfg.spi_slave_vif;
+        spi_mntr.spi_slave_vif=spi_cfg.spi_slave_vif;
         if(spi_cfg.is_active==UVM_ACTIVE)
             begin
-                spi_driver.spi_slave_vif=spi_cfg.spi_slave_vif;
-                spi_driver.seq_item_port.connect(spi_sequencer.seq_item_export);    
+                spi_drvr.spi_slave_vif=spi_cfg.spi_slave_vif;
+                spi_drvr.seq_item_port.connect(spi_sqr.seq_item_export);    
             end
 
-        spi_monitor.mon_ap.connect(spi_agt_ap);
+        spi_mntr.mon_ap.connect(spi_agt_ap);
     endfunction 
 endclass //className extends superClass
