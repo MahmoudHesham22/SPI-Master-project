48cdf0df37c00c5a43b94f73e6a8649f635475f5
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-14 15:12:08 +0300

    Update apb_interface.sv

--- Environment/interfaces/apb_interface.sv
+++ Environment/interfaces/apb_interface.sv
@@ -1,5 +1,7 @@
-interface apb_if (input logic PCLK);
-    
+`timescale 1ns/1ps
+
+interface apb_if (PCLK);
+    input logic PCLK;
     logic       PRESETn;
     logic        PSEL;
     logic        PENABLE;
@@ -10,13 +12,28 @@ interface apb_if (input logic PCLK);
     logic        PREADY;
     logic        PSLVERR;
 
+    // --------------- Driver-side clocking block ----------------------------
+    clocking cb_master @(posedge PCLK);
+        default input #1step output #1;
+        output PSEL, PENABLE, PWRITE, PADDR, PWDATA, PRESETn;
+        input PRDATA, PREADY, PSLVERR;
+    endclocking
+
+    // --------------- Monitor-side clocking block ----------------------------
+    clocking cb_monitor @(posedge PCLK);
+        default input #1step;
+        input PSEL, PENABLE, PWRITE, PADDR, PWDATA, PRESETn;
+        input PRDATA, PREADY, PSLVERR;
+    endclocking
                      
     modport DUT (input PCLK, PRESETn, PSEL, PENABLE, PWRITE, PADDR, PWDATA, 
                      output PRDATA, PREADY, PSLVERR);
 
-    modport monitor (input PCLK, PRESETn, PSEL, PENABLE, PWRITE, PADDR, PWDATA, PRDATA, PREADY, PSLVERR);
+    modport monitor (input PCLK, PRESETn, PSEL, PENABLE, PWRITE, PADDR, PWDATA, PRDATA, PREADY, PSLVERR,
+                     clocking cb_monitor);
 
     modport driver (input PCLK, PRESETn,
-                     output PSEL, PENABLE, PWRITE, PADDR, PWDATA);
+                     output PSEL, PENABLE, PWRITE, PADDR, PWDATA,
+                     clocking cb_master);
                      
 endinterface : apb_if
