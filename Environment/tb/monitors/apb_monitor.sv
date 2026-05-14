c96a49cba899bab2f135cfa00adac518ab86ba3a
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-14 15:14:29 +0300

    Update apb_monitor.sv

--- Environment/tb/monitors/apb_monitor.sv
+++ Environment/tb/monitors/apb_monitor.sv
@@ -1,3 +1,13 @@
+//------------------------------------------------------------------------------
+// Title      : SPI Master Project
+// Project    : SPI Master
+// File       : apb_monitor.sv
+//------------------------------------------------------------------------------
+// Description: This file contains the implementation of the APB monitor class, which is responsible for monitoring the APB signals based on the sequence items received from the sequencer. The monitor interacts with the APB
+//              slave interface to perform the necessary operations for the APB communication using clocking blocks for synchronization.
+//------------------------------------------------------------------------------
+
+
 class apb_monitor extends uvm_monitor;
      `uvm_component_utils(apb_monitor)
     apb_sequence_item apb_item;
@@ -17,13 +27,19 @@ class apb_monitor extends uvm_monitor;
         super.run_phase(phase);
         forever begin
             apb_item = apb_sequence_item::type_id::create("apb_item");
-            @(negedge apb_vif.clk);
-            apb_item.PSEL = apb_vif.PSEL;
-            apb_item.PENABLE = apb_vif.PENABLE;
-            apb_item.PWRITE = apb_vif.PWRITE;
-            apb_item.PADDR = apb_vif.PADDR;
-            apb_item.PWDATA = apb_vif.PWDATA;
-            apb_item.PRDATA = apb_vif.PRDATA;
+            
+            // Sample signals through clocking block
+            @(apb_vif.cb_monitor);
+            apb_item.PRESETn = apb_vif.PRESETn;
+            apb_item.PSEL = apb_vif.cb_monitor.PSEL;
+            apb_item.PENABLE = apb_vif.cb_monitor.PENABLE;
+            apb_item.PWRITE = apb_vif.cb_monitor.PWRITE;
+            apb_item.PADDR = apb_vif.cb_monitor.PADDR;
+            apb_item.PWDATA = apb_vif.cb_monitor.PWDATA;
+            apb_item.PRDATA = apb_vif.cb_monitor.PRDATA;
+            apb_item.PREADY = apb_vif.cb_monitor.PREADY;
+            apb_item.PSLVERR = apb_vif.cb_monitor.PSLVERR;
+            
             apb_mon_ap.write(apb_item);
         end
     endtask //
