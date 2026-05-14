3f57b9c3943cb6b004d67baf2282d043d026cc95
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-05-14 15:12:42 +0300

    Update spi_interface.sv

--- Environment/interfaces/spi_interface.sv
+++ Environment/interfaces/spi_interface.sv
@@ -1,18 +1,34 @@
-interface spi_if (input logic PCLK);
+`timescale 1ns/1ps
 
-    logic      PRESETn;
+interface spi_if (PCLK);
+    input logic PCLK;
     logic        SCLK;          
     logic        MOSI;          
     logic        MISO;          
     logic [3:0]  SS_n;          
     logic        IRQ;           
 
-    modport DUT  (input PCLK, PRESETn, MISO,
+    // --------------- Slave driver-side clocking block ----------------------------
+    clocking cb_slave @(posedge PCLK);
+        default input #1step output #0;
+        input SCLK, MOSI, SS_n, IRQ;
+        output MISO;
+    endclocking
+
+    // --------------- Monitor-side clocking block ----------------------------
+    clocking cb_mon @(posedge PCLK);
+        default input #1step;
+        input SCLK, MOSI, MISO, SS_n, IRQ;
+    endclocking
+
+    modport DUT  (input PCLK, MISO,
                      output SCLK, MOSI, SS_n, IRQ);
 
-    modport monitor (input PCLK, PRESETn, SCLK, MOSI, MISO, SS_n, IRQ);
+    modport monitor (input PCLK, SCLK, MOSI, MISO, SS_n, IRQ,
+                     clocking cb_mon);
 
-    modport driver (input PCLK, PRESETn,
-                     output SCLK, MOSI, SS_n, IRQ);
+    modport driver (input PCLK, MISO,
+                     output SCLK, MOSI, SS_n, IRQ,
+                     clocking cb_slave);
 
 endinterface : spi_if
